14a0588ddd61630861be96e7d32465cab0a74b6e
Wanderson Caldas <wcaldasti@gmail.com>
2020-12-25 19:29:37 -0300

    CRIAR PROJETO

---
+++ eBasicAuthentication/src/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace src.Controllers
+{
+    [Authentication.BasicAuthentication]
+    public class HomeController : ApiController
+    {
+        // GET: api/Home                
+        public IHttpActionResult Get()
+        {
+            return Ok();
+        }
+
+        // GET: api/Home/5        
+        public IHttpActionResult Get(int id)
+        {
+            return Ok();
+        }
+
+        // POST: api/Home        
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Home/5        
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Home/5        
+        public void Delete(int id)
+        {
+        }
+    }
+}
